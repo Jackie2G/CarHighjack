fae69877f02f0dbd8797395ea42a2ed18573e8c0
quemzys-hash <quemzys@gmail.com>
2026-05-11 12:37:32 +0200

    Dodano walidacje do Car, Booking i Trip

--- CarHighjack.Domain/Booking.cs
+++ CarHighjack.Domain/Booking.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using CarHighjack.Domain;
 
 namespace CarHighjack.Domain
 {
@@ -7,15 +6,30 @@ namespace CarHighjack.Domain
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "ID uzytkownika jest wymagane")]
         public string? UserId { get; set; }
+
+        [Required(ErrorMessage = "VIN samochodu jest wymagany")]
         public string? CarVIN { get; set; }
+
+        [Required(ErrorMessage = "Data rozpoczecia jest wymagana")]
         public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "Data zakonczenia jest wymagana")]
         public DateTime EndDate { get; set; }
+
         public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Koszt projektu jest wymagany")]
         public string? ProjectCost { get; set; }
+
         public DateTime CarTakenUTC { get; set; }
         public DateTime CarReturnedUTC { get; set; }
+
+        [Range(0, 3, ErrorMessage = "Status rezerwacji musi byc miedzy 0 a 3")]
         public int BookingStatus { get; set; }
+
         public Car? Car { get; set; }
     }
 }
--- CarHighjack.Domain/Trip.cs
+++ CarHighjack.Domain/Trip.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using CarHighjack.Domain;
 
 namespace CarHighjack.Domain
 {
@@ -7,17 +6,34 @@ namespace CarHighjack.Domain
     {
         [Key]
         public int Id { get; set; }
+
         public bool Active { get; set; }
+
+        [Range(0, long.MaxValue, ErrorMessage = "Przebieg startowy nie moze byc ujemny")]
         public long StartMileage { get; set; }
+
+        [Required(ErrorMessage = "Lokalizacja startowa jest wymagana")]
         public string? StartLocation { get; set; }
+
         public DateTime StartUTCTime { get; set; }
+
+        [Range(0, long.MaxValue, ErrorMessage = "Przebieg koncowy nie moze byc ujemny")]
         public long EndMileage { get; set; }
+
         public string? EndLocation { get; set; }
+
         public DateTime EndUTCTime { get; set; }
+
         public string? Project { get; set; }
+
+        [Range(0, float.MaxValue, ErrorMessage = "Koszt nie moze byc ujemny")]
         public float Cost { get; set; }
+
         public string? CostRemarks { get; set; }
+
+        [Required(ErrorMessage = "ID rezerwacji jest wymagane")]
         public int BookingId { get; set; }
+
         public Booking? Booking { get; set; }
     }
 }
